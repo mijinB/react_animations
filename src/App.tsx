d68430a92a9b58c74b5b33e08f34c905b9599c62
mijinB <bmj13465@gmail.com>
2023-09-07 19:17:41 +0900

    Variants 사용해서 자식 요소에 animation 적용

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,62 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
     width: 200px;
     height: 200px;
-    border-radius: 15px;
+    border-radius: 40px;
+    background-color: rgba(255, 255, 255, 0.2);
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+    place-self: center;
+    width: 70px;
+    height: 70px;
+    border-radius: 35px;
     background-color: white;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-    start: { scale: 0 },
-    end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+const boxVariants = {
+    start: {
+        scale: 0.5,
+        opacity: 0,
+    },
+    end: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            duration: 1,
+            bounce: 0.5,
+            delayChildren: 0.5,
+            staggerChildren: 0.2,
+        },
+    },
+};
+
+const circleVariants = {
+    start: {
+        opacity: 0,
+        y: 10,
+    },
+    end: {
+        opacity: 1,
+        y: 0,
+    },
 };
 
 function App() {
     return (
         <Wrapper>
-            <Box variants={myVars} initial="start" animate="end" />
+            <Box variants={boxVariants} initial="start" animate="end">
+                <Circle variants={circleVariants} />
+                <Circle variants={circleVariants} />
+                <Circle variants={circleVariants} />
+                <Circle variants={circleVariants} />
+            </Box>
         </Wrapper>
     );
 }
