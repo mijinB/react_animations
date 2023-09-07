1fc3f85158075f84c3189f66bdfa9ed289abea82
mijinB <bmj13465@gmail.com>
2023-09-08 00:12:25 +0900

    useTransform 사용해서 x값에 따라 background 색상변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100vw;
     height: 100vh;
+    background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
@@ -19,11 +20,19 @@ const Box = styled(motion.div)`
 
 function App() {
     const x = useMotionValue(0);
-    const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+    const gradient = useTransform(
+        x,
+        [-800, 800],
+        [
+            "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+            "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+        ]
+    );
 
     return (
-        <Wrapper>
-            <Box drag="x" dragSnapToOrigin style={{ x, scale }} />
+        <Wrapper style={{ background: gradient }}>
+            <Box drag="x" dragSnapToOrigin style={{ x, rotateZ }} />
         </Wrapper>
     );
 }
