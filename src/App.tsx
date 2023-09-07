bd9b8f4d6c58256bae3aa7f1955197c41b852773
mijinB <bmj13465@gmail.com>
2023-09-07 19:20:10 +0900

    자동 완성 기능을 위해 Variants type 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const Wrapper = styled.div`
     display: flex;
@@ -28,7 +28,7 @@ const Circle = styled(motion.div)`
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
+const boxVariants: Variants = {
     start: {
         scale: 0.5,
         opacity: 0,
@@ -46,7 +46,7 @@ const boxVariants = {
     },
 };
 
-const circleVariants = {
+const circleVariants: Variants = {
     start: {
         opacity: 0,
         y: 10,
