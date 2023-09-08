c21970d901cf179b12da471bdb59a5882e154872
mijinB <bmj13465@gmail.com>
2023-09-08 11:15:31 +0900

    Slider next

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 100vw;
@@ -13,43 +14,63 @@ const Wrapper = styled(motion.div)`
 const Box = styled(motion.div)`
     position: absolute;
     top: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 400px;
     height: 200px;
     border-radius: 40px;
     background-color: rgba(255, 255, 255, 1);
+    font-size: 28px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-    initial: {
+const box = {
+    invisible: {
+        x: 500,
         opacity: 0,
         scale: 0,
     },
     visible: {
+        x: 0,
         opacity: 1,
         scale: 1,
-        rotateZ: 360,
+        transition: {
+            duration: 1,
+        },
     },
-    leaving: {
+    exit: {
+        x: -500,
         opacity: 0,
         scale: 0,
-        y: 50,
+        transition: {
+            duration: 1,
+        },
     },
 };
 
 function App() {
-    const [showing, setShowing] = useState(false);
+    const [visible, setVisible] = useState(1);
 
-    const toggleShowing = () => {
-        setShowing((prev) => !prev);
+    /**@function nextPlease
+     * 1. 값이 10이면 10을 반환하고 10이 아니면 +1한 값을 반환
+     */
+    const nextPlease = () => {
+        setVisible((prev) => (prev === 10 ? 10 : prev + 1));
     };
 
     return (
         <Wrapper>
-            <button onClick={toggleShowing}>Click</button>
             <AnimatePresence>
-                {showing ? <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving" /> : null}
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+                    i === visible ? (
+                        <Box key={i} variants={box} initial="invisible" animate="visible" exit="exit">
+                            {i}
+                        </Box>
+                    ) : null
+                )}
             </AnimatePresence>
+            <button onClick={nextPlease}>next</button>
         </Wrapper>
     );
 }
