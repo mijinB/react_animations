2fb7f282645965a120ccb2229780824add4128a9
mijinB <bmj13465@gmail.com>
2023-09-08 14:20:20 +0900

    layoutId 사용해서 4개 box 클릭하면 가운데로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -42,29 +42,25 @@ const Overlay = styled(motion.div)`
 `;
 
 function App() {
-    const [clicked, setClicked] = useState(false);
-
-    /**@function toggle
-     * 1. clicked의 bool 값을 toggle
-     */
-    const toggle = () => setClicked((prev) => !prev);
+    const [id, setId] = useState<null | string>(null);
+    console.log(id);
 
     return (
-        <Wrapper onClick={toggle}>
+        <Wrapper>
             <Grid>
-                <Box layoutId="hello" />
-                <Box />
-                <Box />
-                <Box />
+                {["1", "2", "3", "4"].map((n) => (
+                    <Box key={n} onClick={() => setId(n)} layoutId={n} />
+                ))}
             </Grid>
             <AnimatePresence>
-                {clicked ? (
+                {id ? (
                     <Overlay
                         initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
                         animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
                         exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                        onClick={() => setId(null)}
                     >
-                        <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+                        <Box layoutId={id} style={{ width: 400, height: 200 }} />
                     </Overlay>
                 ) : null}
             </AnimatePresence>
