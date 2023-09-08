2f6a3829cfa6f52db52aa9ad3c02e1949372ddf2
mijinB <bmj13465@gmail.com>
2023-09-08 16:22:29 +0900

    Hover된 컴포넌트 크기가 커지는 motion(위치에 따라 방향 다름)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, easeInOut } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled.div`
@@ -12,24 +12,8 @@ const Wrapper = styled.div`
 
 const Grid = styled.div`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
     gap: 10px;
-    width: 50vw;
-    div:first-child,
-    div:last-child {
-        grid-column: span 2;
-    }
-`;
-
-const Box = styled(motion.div)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 200px;
-    border-radius: 40px;
-    background-color: rgba(255, 255, 255, 1);
-    font-size: 28px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const Overlay = styled(motion.div)`
@@ -41,28 +25,56 @@ const Overlay = styled(motion.div)`
     height: 100%;
 `;
 
+const Box = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 300px;
+    height: 200px;
+    border-radius: 8px;
+    background-color: rgba(255, 255, 255, 0.5);
+    font-size: 28px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+    width: 70px;
+    height: 70px;
+`;
+
+const boxVariants = {
+    hover: (isLeft: boolean) => {
+        return {
+            scale: 1.1,
+            x: isLeft ? -15 : 15,
+            y: isLeft ? -10 : 10,
+        };
+    },
+};
+
 function App() {
+    const [isLeft, setIsLeft] = useState(true);
     const [id, setId] = useState<null | string>(null);
-    console.log(id);
 
     return (
         <Wrapper>
-            <Grid>
-                {["1", "2", "3", "4"].map((n) => (
-                    <Box key={n} onClick={() => setId(n)} layoutId={n} />
-                ))}
-            </Grid>
-            <AnimatePresence>
-                {id ? (
-                    <Overlay
-                        initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-                        animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-                        exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-                        onClick={() => setId(null)}
-                    >
-                        <Box layoutId={id} style={{ width: 400, height: 200 }} />
-                    </Overlay>
-                ) : null}
+            <AnimatePresence custom={isLeft}>
+                <Grid>
+                    {[1, 2, 3, 4].map((i) =>
+                        i === 1 || i === 4 ? (
+                            <Box
+                                key={i}
+                                variants={boxVariants}
+                                custom={isLeft}
+                                whileHover="hover"
+                                transition={{ duration: 0.1 }}
+                                onMouseEnter={() => (i === 1 ? setIsLeft(true) : setIsLeft(false))}
+                            />
+                        ) : (
+                            <Box key={i} />
+                        )
+                    )}
+                </Grid>
             </AnimatePresence>
         </Wrapper>
     );
