e3e8e4072d3b2dc1777db281579662cadb3e3467
mijinB <bmj13465@gmail.com>
2023-09-08 18:34:54 +0900

    선택된 Box 컴포넌트를 중앙으로 이동하며 Overlay가 깔리는 motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
-import { motion, AnimatePresence, easeInOut } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
     width: 100vw;
     height: 100vh;
@@ -19,6 +19,7 @@ const Grid = styled.div`
 
 const Overlay = styled(motion.div)`
     position: absolute;
+    z-index: 1;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -43,7 +44,21 @@ const Circle = styled(motion.div)`
     height: 50px;
     border-radius: 25px;
     background-color: rgba(255, 255, 255, 1);
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.4), 0 10px 20px rgba(0, 0, 0, 0.06);
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.4);
+`;
+
+const Switch = styled(motion.button)`
+    position: absolute;
+    bottom: 170px;
+    margin-top: 50px;
+    padding: 7px;
+    border: none;
+    border-radius: 5px;
+    background-color: white;
+    font-weight: 600;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.2);
+    color: rgb(0, 0, 255);
+    cursor: pointer;
 `;
 
 const boxVariants = {
@@ -53,6 +68,13 @@ const boxVariants = {
             x: isLeftHover ? -15 : 15,
             y: isLeftHover ? -10 : 10,
         };
+    },
+};
+
+const switchVariants = {
+    click: {
+        scale: 1.2,
+        color: "rgb(255, 99, 71)",
     },
 };
 
@@ -70,21 +92,23 @@ function App() {
 
     return (
         <Wrapper>
-            <AnimatePresence custom={isLeftHover}>
+            <AnimatePresence>
                 <Grid key="grid">
-                    {[1, 2, 3, 4].map((i) =>
-                        i === 1 || i === 4 ? (
+                    {["1", "2", "3", "4"].map((i) =>
+                        i === "1" || i === "4" ? (
                             <Box
                                 key={i}
+                                layoutId={i}
                                 variants={boxVariants}
                                 custom={isLeftHover}
                                 whileHover="hover"
                                 transition={{ duration: 0.1 }}
-                                onMouseEnter={() => (i === 1 ? setIsLeftHover(true) : setIsLeftHover(false))}
+                                onMouseEnter={() => (i === "1" ? setIsLeftHover(true) : setIsLeftHover(false))}
+                                onClick={() => setId(i)}
                             />
                         ) : (
                             <Box key={i}>
-                                {i === 2 ? (
+                                {i === "2" ? (
                                     isRightCircle ? (
                                         <Circle layoutId="circle" />
                                     ) : null
@@ -95,7 +119,24 @@ function App() {
                         )
                     )}
                 </Grid>
-                <button onClick={toggleSwitch}>Switch</button>
+                {id ? (
+                    <Overlay
+                        key="overlay"
+                        initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                        animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+                        exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                        onClick={() => setId(null)}
+                    >
+                        <Box layoutId={id} style={{ backgroundColor: "rgba(255, 255, 255, 1)" }} />
+                    </Overlay>
+                ) : null}
+                {isRightCircle ? (
+                    <Switch onClick={toggleSwitch}>Switch</Switch>
+                ) : (
+                    <Switch variants={switchVariants} animate="click" onClick={toggleSwitch}>
+                        Switch
+                    </Switch>
+                )}
             </AnimatePresence>
         </Wrapper>
     );
